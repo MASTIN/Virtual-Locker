469125660aa44d14b07948a8651791f3d9b972ff
Mary Morvan <mamorvan@yahoo.com>
2017-06-04 14:33:27 -0400

    add sequelize to server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+
+var db = require("./models");
+//sequelize sync
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("Listening on port: " + PORT);
+    });
+});
