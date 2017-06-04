2f377c10ac6eb8c1ceb3df54659ff27f52653e61
Mary Morvan <mamorvan@yahoo.com>
2017-06-04 15:01:25 -0400

    create basic models for item and user :ring: :family:

---
+++ models/user.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    });
+    return User;
+}
