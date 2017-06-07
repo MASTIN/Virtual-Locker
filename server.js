b4b8c36a87229682e10ce647bf04c29d3087d316
Stuart Schafer <stuart_schafer@hotmail.com>
2017-06-07 02:14:29 -0400

    :rage: Got it creating new entries to the db but having issues with validation

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
 //set up static directory for all public files
-app.use(express.static(path.join(__dirname + "./public")));
+// app.use(express.static(path.join(__dirname + "./public")));
+app.use(express.static("./public"));
 
 //*****ROUTES*****//
 require("./routes/html-routes.js")(app);
