aaacf2391580ebd49d06619788b885dda1798d7f
Stuart Schafer <stuart_schafer@hotmail.com>
2017-06-06 23:43:55 -0400

    :foggy:Updated the add.html for id's and added a few routes to post routes file

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 app.use(express.static(path.join(__dirname + "./public")));
 
 //*****ROUTES*****//
-
+require("./routes/html-routes.js")(app);
+require("./routes/post-routes.js")(app);
 
 //sequelize sync models then start express app
 // This we'll use if we need to change the tables
