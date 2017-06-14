ea83939444cb65209207643d162efd1a4527f6f9
Mary Morvan <mamorvan@yahoo.com>
2017-06-14 16:10:32 -0400

    ok really stopping now :mouse:

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,6 @@
 +var createErrorMessages = function() {
  +    var form = $(this),
- +    errorList = $("ul.errorMessages", form);
- +
- +    var showAllErrorMessages = function() {
- +        errorList.empty();
- +
- +        //Find all invalid fields in form
- +        var invalidFields = form.find(":invalid").each(function(index, node){
- +            //Find field's label
- +            var label = $("label[for=" + node.id +"]"),
- +            message = node.validationMessage || "Please fill in a valid value";
- +
- +            errorList.show().append("<li><span>" + label.html() + "</span>" + message + "</li>");
- +        });
- +    };
- +    
+
  +    //for Safari
  +    form.on( "submit", function(event){
  +        if (this.checkValidity && !this.checkValidity()) {
@@ -22,10 +8,7 @@
  +            event.preventDefault();
  +        }
  +    });
- +
- +    $("input[type=submit], button:not([type=button])", form)
- +    .on("click", showAllErrorMessages);
- +
+
  +    $("input", form).on()
  +};
  +
