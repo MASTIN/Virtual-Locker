2e7ad0b2b75abf98222caf0775a379a5fa52a675
Mary Morvan <mamorvan@yahoo.com>
2017-06-14 15:33:51 -0400

    bit more work on sign in /up validations :sweat_smile:

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,13 @@
+//check inputs on sign in and sign up forms are valid using html5 form validation
 var createErrorMessages = function() {
     var form = $(this),
-    errorList = $("ul.errorMessages", form);
 
-    var showAllErrorMessages = function() {
-        errorList.empty();
-
-        //Find all invalid fields in form
-        var invalidFields = form.find(":invalid").each(function(index, node){
-            //Find field's label
-            var label = $("label[for=" + node.id +"]"),
-            message = node.validationMessage || "Please fill in a valid value";
-
-            errorList.show().append("<li><span>" + label.html() + "</span>" + message + "</li>");
-        });
-    };
-    
-    //for Safari
     form.on( "submit", function(event){
         if (this.checkValidity && !this.checkValidity()) {
             $(this).find(":invalid").first().focus();
             event.preventDefault();
         }
     });
-
-    $("input[type=submit], button:not([type=button])", form)
-    .on("click", showAllErrorMessages);
 
     $("input", form).on()
 };
